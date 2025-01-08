9c0558bc8f0640e3981b30238cda4394177e1268
kelvinnicolau <kelvinnicolau7@gmail.com>
2025-01-08 14:25:14 -0300

    feat: adc google analytics

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import ReactGA from 'react-ga4';
+
+// Inicializa o Google Analytics
+ReactGA.initialize('G-QPLKYPHRSZ'); // Substitua pelo seu ID de acompanhamento
+
+// Componente para rastrear mudanças de rota
+const AnalyticsTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, [location]);
+
+  return null;
+};
+
+// Componente principal com o rastreamento
+const MainApp = () => (
+  <BrowserRouter>
+    <AnalyticsTracker />
+    <App />
+  </BrowserRouter>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MainApp />
   </React.StrictMode>,
   document.getElementById('root')
-)
-
-
+);
